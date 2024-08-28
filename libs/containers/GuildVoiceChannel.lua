45c6756b4aaaec25150cd1d23d99fc52cfb8a6cd
BestMordaEver <matvey@fedyay.net>
2024-08-28 12:54:00 +0300

    properly loading the multiple parent class

--- libs/containers/GuildVoiceChannel.lua
+++ libs/containers/GuildVoiceChannel.lua
@@ -18,6 +18,11 @@ function GuildVoiceChannel:__init(data, parent)
 	TextChannel.__init(self, data, parent)
 end
 
+function GuildVoiceChannel:_load(data)
+	GuildChannel._load(self, data)
+	TextChannel._load(self, data)
+end
+
 --[=[
 @m setBitrate
 @t http
